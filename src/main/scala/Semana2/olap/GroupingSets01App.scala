dc27e342b36c118da87f5149164b137410c0f1be
mario-renau-eoi <mrenau@me.com>
2025-02-28 10:11:29 +0100

    Refactor GroupingSets app and add reusable utility methods
    Extracted reusable utility functions into a new object to improve modularity and testability. Introduced a new test suite to validate these methods for correctness and coverage. Added configuration, DataFrame creation, SQL query execution, and rollup operations as separate methods.

--- src/main/scala/Semana2/olap/GroupingSets01App.scala
+++ src/main/scala/Semana2/olap/GroupingSets01App.scala
@@ -1,8 +1,7 @@
 package Semana2.olap
 
 import Semana2.SparkSessionWrapper
-
-import org.apache.spark.sql.functions
+import org.apache.spark.sql.{DataFrame, SparkSession, functions}
 
 object GroupingSets01App extends App with SparkSessionWrapper {
 
@@ -22,7 +21,7 @@ object GroupingSets01App extends App with SparkSessionWrapper {
     ("Orange", "Fruit", 2000, 2),
     ("Banana", "Fruit", 4000, 3),
     ("Carrot", "Vegetable", 4000, 3),
-    ("Bean", "Vegetable", 3000, 0 ),
+    ("Bean", "Vegetable", 3000, 0),
     ("Orange", "Fruit", 3000, 0)
   )
 
@@ -45,3 +44,66 @@ object GroupingSets01App extends App with SparkSessionWrapper {
   result.show(false)
 }
 
+// Meto las funciones en un objeto para poder reutilizarlas y testearlas
+object GroupingSets01V2 {
+  // Utility methods
+  def configureSpark(spark: SparkSession): Unit = {
+    spark.conf.set("spark.sql.adaptive.enabled", "false") // Disable AQE
+    spark.conf.set("spark.sql.autoBroadcastJoinThreshold", "-1") // Disable broadcast join
+  }
+
+  def createDataFrame(data: Seq[(String, String, Int, Int)])(implicit spark: SparkSession): DataFrame = {
+    import spark.implicits._
+    val df = data.toDF("Name", "Category", "Sales", "Quantity")
+    df
+  }
+
+  def performSqlQuery(df: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    df.createOrReplaceTempView("sales_data")
+    spark.sql(
+      """
+        |SELECT Name, Category, SUM(Sales)
+        |FROM sales_data
+        |GROUP BY Name, Category GROUPING SETS((Name, Category))
+      """.stripMargin)
+  }
+
+  def performDataFrameOperations(df: DataFrame): DataFrame = {
+    df.groupBy("Name", "Category")
+      .agg(functions.sum("Sales"))
+      .rollup("Name", "Category")
+      .count()
+  }
+}
+
+object GroupingSets01V2App extends App with SparkSessionWrapper {
+
+  import GroupingSets01V2._
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  // Prepare data
+  val data = Seq(
+    ("Banana", "Fruit", 1000, 1),
+    ("Carrot", "Vegetable", 1000, 1),
+    ("Bean", "Vegetable", 2000, 2),
+    ("Orange", "Fruit", 2000, 2),
+    ("Banana", "Fruit", 4000, 3),
+    ("Carrot", "Vegetable", 4000, 3),
+    ("Bean", "Vegetable", 3000, 0),
+    ("Orange", "Fruit", 3000, 0)
+  )
+  val df = createDataFrame(data)
+
+  // Perform operations
+  val sqlResult = performSqlQuery(df)
+  val dfResult = performDataFrameOperations(df)
+
+  // Show results
+  sqlResult.explain(true)
+  dfResult.explain(true)
+  sqlResult.show(false)
+  dfResult.show(false)
+
+
+}
